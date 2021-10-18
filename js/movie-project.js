61a236d01e59d1e2ad0d8a12e4eb7728c331cef7
Alex Riley <alex.riley.ar12@gmail.com>
2021-10-18 09:25:57 -0500

    just getting started

--- js/movie-project.js
+++ js/movie-project.js
@@ -1,1 +1,6 @@
-fetch('https://prong-lead-fold.glitch.me/movies')
+const movieAPIURL = 'https://prong-lead-fold.glitch.me/movies'
+
+function getMovies(){
+    return fetch(movieAPIURL)
+        .then((response)=>console.log(response.json()));
+}
