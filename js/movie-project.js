0c83e38a81e098170200161f8f7be8c2776fdebc
Alex Riley <alex.riley.ar12@gmail.com>
2021-10-18 10:59:41 -0500

    captured movies array, and displaying individual movies

--- js/movie-project.js
+++ js/movie-project.js
@@ -1,6 +1,27 @@
 const movieAPIURL = 'https://prong-lead-fold.glitch.me/movies'
+const button = document.getElementsByClassName('newMovie')
+$(document ).ready(function(){
+    alert("Loading...")
+})
 
-function getMovies(){
-    return fetch(movieAPIURL)
-        .then((response)=>console.log(response.json()));
+
+// function getMovies(){
+    fetch(movieAPIURL)
+        .then((response)=> {return response.json()})
+        .then((movieData) =>{
+            console.log(movieData);
+            movieData.forEach((movie)=>{console.log(movie.title)})
+        })
+// }
+// getMovies();
+
+
+
+button.addEventListener('click', function(e){
+    e.preventDefault();
+    let newMovie = $('.newMovie').val();
+})
+
+for(let i=0; i < movieData.length; i++){
+    $('.movies').html(movieData[i])
 }
