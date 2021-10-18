283107766c7e4a1eff701d75b400f84c9044e0d7
Destiney Rodney <destiney.rodney01@gmail.com>
2021-10-18 12:06:53 -0500

    feat: Continued working on adding user movie functionality.

--- js/movie-project.js
+++ js/movie-project.js
@@ -1,32 +1,69 @@
 const movieAPIURL = 'https://prong-lead-fold.glitch.me/movies'
 const button = document.getElementsByClassName('newMovie')
-$(document ).ready(function(){
+$(document).ready(function () {
     alert("Loading...")
-})
+
+
+let submitMovie = document.querySelector("#submitMovie") //To get button to work
+
 
 
 // function getMovies(){
-    fetch(movieAPIURL)
-        .then((response)=> {return response.json()})
-        .then((movieData) =>{
-            console.log(movieData);
-            for(let i=0; i < movieData.length; i++){
+fetch(movieAPIURL)
+    .then((response) => {
+        return response.json()
+    })
+    .then((movieData) => {
+        console.log(movieData);
+        for (let i = 0; i < movieData.length; i++) {
 
-            }
-            movieData.forEach((movie)=>{
-                console.log(movie.title)
-                $('#movies').append(`<h3>${movie.title}</h3>
-                <div>${movie.rating} stars</div>`)
-            })
+        }
+
+
+        //    SEARCH BUTTON
+        submitMovie.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            let newMovieTitle = $('.newMovieTitle').val();
+            let newMovieRating = $('.newMovieRating').val();
+
+            var movieObject =
+                {
+                    title: newMovieTitle,
+                    rating: newMovieRating,
+                }
 
         })
+
+        movieData.forEach((movie) => {
+            console.log(movie.title)
+            $('#movies').append(`<h3>Movie Title: ${movie.title.charAt(0).toUpperCase() + movie.title.slice(1)}</h3>
+                <div>Rating: ${movie.rating} stars</div>
+                <div>Actors: ${movie.actors} </div>
+                <div>Year: ${movie.year}</div>
+               
+`)
+
+
+        })
+
+
+    })
 // }
 // getMovies();
 
 
-
 // button.addEventListener('click', function(e){
 //     e.preventDefault();
-//     let newMovie = $('.newMovie').val();
+//     let newMovieTitle = $('.newMovieTitle').val();
+//     let newMovieRating = $('.newMovieRating').val();
+//
+// var movieobject =
+//     {
+//         title: newMovieTitle,
+//         rating: newMovieRating,
+//     }
+//
 // })
 
+})
